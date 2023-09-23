e3878ba897ff5ae7f7270d5f8a95dd0427729065
matte-ek <matero3210@gmail.com>
2023-09-23 19:08:22 +0200

    Add support for calculating a specific score
    Support as in duct-taping stuff together :p

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,57 @@ fn main() {
         Err(_) => return println!("err: failed to load map."),
     };
 
-    let result100 = map.pp()
-        .accuracy(100.0)
-        .calculate(); 
+    if args.len() <= 3 {
+        let mods: u32 = if args.len() == 3 {
+            args[2].parse().unwrap()
+        } else {
+            0
+        };
 
-    let result98 = map.pp()
-        .accuracy(98.0)
-        .calculate(); 
+        let result100 = map.pp()
+            .accuracy(100.0)
+            .mods(mods)
+            .calculate(); 
 
-    let result95 = map.pp()
-        .accuracy(95.0)
-        .calculate(); 
+        let result98 = map.pp()
+            .accuracy(98.0)
+            .mods(mods)
+            .calculate(); 
 
-    println!("{}", result100.pp());
-    println!("{}", result98.pp());
-    println!("{}", result95.pp());
+        let result95 = map.pp()
+            .accuracy(95.0)
+            .mods(mods)
+            .calculate(); 
+
+        println!("{}", result100.pp());
+        println!("{}", result98.pp());
+        println!("{}", result95.pp());
+    }
+    else if args.len() <= 8 { // <map-id> <n300> <n100> <n50> <nmiss> <max-combo> <[mods]>
+        let mods: u32 = if args.len() == 8 {
+            args[7].parse().unwrap()
+        } else {
+            0
+        };
+
+        let result = map.pp()
+            .n300(args[2].parse().unwrap())
+            .n100(args[3].parse().unwrap())
+            .n50(args[4].parse().unwrap())
+            .n_misses(args[5].parse().unwrap())
+            .combo(args[6].parse().unwrap())
+            .mods(mods)
+            .calculate();
+
+        let result_max = map.pp()
+            .n300(args[2].parse().unwrap())
+            .n100(args[3].parse().unwrap())
+            .n50(args[4].parse().unwrap())
+            .n_misses(0)
+            .mods(mods)
+            .calculate();
+
+        println!("{}", result.pp());
+        println!("{}", result_max.pp());
+    }
 }
